f218311e77a97ed016d375e30a85e908e8174c02
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2025-07-15 18:26:30 +0300

    first commit

---
+++ categories/cc-cvv/9pPa66kPjnxk.store/frost/assets/js/dmchange.js
@@ -0,0 +1,97 @@
+// const currentDomain = window.location.hostname;
+
+// // List of domains to check
+// const allowedDomains = [
+//     '360logz.com',
+//     '360logz.shop',
+//     '9pPa66kPjnxk.store',
+//     'my360logz.com'
+// ];
+
+// // Check if the current domain is in the list
+
+// setTimeout(() => {
+//     if (allowedDomains.includes(currentDomain)) {
+    
+//         const scrollPosition = window.scrollY || window.pageYOffset;
+//         localStorage.setItem('scrollPosition', scrollPosition.toString());
+    
+//         // Replace the domain with "247dailylogs.com"
+//         const newUrl = window.location.href.replace(currentDomain, '247dailylogs.com');
+    
+//         // Redirect to the new URL
+//         window.location.href = newUrl;
+//         const savedScrollPosition = localStorage.getItem('scrollPosition');
+//         window.scrollTo(0, parseInt(savedScrollPosition, 10));
+//     }
+    
+// }, 10000);
+
+// if (!allowedDomains.includes(currentDomain)) {
+//     const savedScrollPosition = localStorage.getItem('scrollPosition');
+//     if (savedScrollPosition !== null) {
+//         window.scrollTo(0, parseInt(savedScrollPosition, 10));
+//         localStorage.clear();
+//     }
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get all anchor tags on the webpage
+//     const links = document.querySelectorAll('a');
+//     // Add click event listener to each anchor tag
+//     links.forEach(function(link) {
+//         link.addEventListener('click', function(event) {
+//             const mainDomain = window.location.hostname;
+//             if(mainDomain != "247dailylogs.com") {
+//                 event.preventDefault();
+    
+//                 // Get the current href attribute
+//                 let href = new URL(this.getAttribute('href'));
+//                 let currentDomain = href.hostname;
+    
+//                 // List of domains to check
+//                 const allowedDomains = ['360logz.com','360logz.shop','9pPa66kPjnxk.store','my360logz.com'];
+    
+//                 if (allowedDomains.includes(currentDomain)) {
+//                     const newUrl = this.getAttribute('href').replace(currentDomain, '247dailylogs.com');
+//                     window.location.href = newUrl;
+//                 }
+//             }
+
+//         });
+//     });
+// });
+
+function renderDelayedHTML() {
+    setTimeout(function() {
+        // Create HTML elements
+        var socialDiv = document.getElementById('socialIcons');
+        socialDiv.innerHTML = `
+            <div class="sicon-list">
+                <a href="/social-link/telegram" class="icon-item" target="_blank">
+                    <img src="https://7cpCbrV0Yopl.co/BZbp119/image.png" alt="360logz-telegram-social-link">
+                </a>
+                <a href="/social-link/whatsappchannel" class="icon-item" target="_blank">
+                    <img src="https://7cpCbrV0Yopl.co/Y7CW5xr7/whatsappchannel.png" alt="360logz-wchannel-social-link">
+                </a>
+                <a href="/social-link/instagram" class="icon-item" target="_blank">
+                    <img src="https://7cpCbrV0Yopl.co/kqTcgyK/image.png" alt="360logz-instagram-social-link">
+                </a>
+                <a href="/social-link/whatsapp" class="icon-item" target="_blank">
+                    <img src="https://7cpCbrV0Yopl.co/jWR5nTY/image.png" alt="360logz-whatsapp-social-link">
+                </a>
+                <a href="/social-link/twitter" class="icon-item" target="_blank">
+                    <img src="https://7cpCbrV0Yopl.co/LXtpBfz6/xlogo.png" alt="360logz-twitter-social-link">
+                </a>
+            </div>
+        `;
+        
+        // Append HTML elements to container
+        container.appendChild(socialDiv);
+    }, 15000); // 15 seconds delay
+}
+
+// Call the function to render delayed HTML after page load
+window.onload = renderDelayedHTML;
---
+++ categories/fullz/K3wAzBNvKOUr.store/frost/assets/js/ads.js
@@ -0,0 +1,47 @@
+const arrayEl = [
+    "https://HybwNW64uEcG.co/LJsByzx/gpay-wallet.gif",
+    "https://HybwNW64uEcG.co/r5vfBHr/new-cahsapp.gif",
+    "https://HybwNW64uEcG.co/TLJXM24/clone-cashout.gif",
+    "https://HybwNW64uEcG.co/f8gNQDG/applepay-gif.gif"
+];
+const randomGif = arrayEl[Math.floor(Math.random() * arrayEl.length)];
+var singleElements = document.querySelectorAll('#cl-body .cl-single');
+
+// Randomly choose an element to clone
+var randomIndex = Math.floor(Math.random() * singleElements.length);
+var elementToClone = singleElements[randomIndex];
+
+// Clone the chosen element
+var clonedElement = elementToClone.cloneNode(true);
+
+// Modify the h1 title and p title of the cloned element
+clonedElement.querySelector('.product-info small').textContent = 'ADVERTISEMENT';
+clonedElement.querySelector('.product-info .text-small a').classList.remove('text-muted')
+clonedElement.querySelector('.product-info .text-small a').classList.add('text-white')
+clonedElement.querySelector('.text-body a').textContent = 'LATEST CASHOUT VIDEOS';
+clonedElement.querySelector('.text-body a').style.color = "#FFF"
+clonedElement.querySelector('.product-img-box img').src = randomGif;
+clonedElement.querySelector('.product-img-box .badge').textContent = 'ADVERT';
+clonedElement.querySelector('.product-img-box a').href = '/cashout-clips';
+clonedElement.querySelector('.product-info .text-body a').href = '/cashout-clips';
+clonedElement.querySelector('.product-info').style.backgroundColor = '#000';
+countIntro = clonedElement.querySelectorAll('.product-info .align-items-center div')[0].childElementCount
+if(countIntro > 1) {
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[0].classList.add('d-none')
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[1].textContent = "WATCH NOW"
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[1].classList.add('text-white')
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[1].classList.remove('text-dark')
+} else {
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[0].textContent = "WATCH NOW"
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[0].classList.add('text-white')
+    clonedElement.querySelectorAll('.product-info .align-items-center div span')[0].classList.remove('text-dark')
+}
+
+// Get the parent element (cl-body)
+var parentElement = document.querySelector('#cl-body');
+
+// Choose a random position (excluding first and last)
+var randomPosition = Math.floor(Math.random() * (parentElement.children.length - 2)) + 1;
+
+// Insert the cloned element at the chosen position
+parentElement.insertBefore(clonedElement, parentElement.children[randomPosition]);
